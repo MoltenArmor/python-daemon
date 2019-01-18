ef319ec48fd372b0caef70c3d74ed322cdf832a2
Hervé Beraud <hberaud@redhat.com>
2019-01-18 14:31:07 +0100

    Refactor packaging by introducing pbr.
    Since python-daemon distribution facing issues
    related to dependances management this commit
    try to avoid this kind of problem and try to
    simplify the project release management by using pbr.
    
    This commit introduce:
    - packaging metadata management by using configuration file;
    - introduce PEP 516;
    - introduce requirements file;
    - convert README to rst format for a better visualization on pagure and better compatibility with pbr.
    
    Using it:
    - install pbr (pip install pbr);
    - create a new tag (git tag 9.9.9)(by example);
    - build and dist (python setup.py build && python setup.py sdist);
    - publish your release on pypi by using twine or whatever you want.
    
    pbr[1] allow simplify python packaging by automatizing:
    - semver[2] management;
    - changelog generation;
    - authors file generation.
    
    [1] https://docs.openstack.org/pbr/latest/
    [2] https://semver.org/
    
    Co-authored-by: Hobbestigrou <hobbestigrou@erakis.eu>

--- setup.py
+++ setup.py
@@ -9,99 +9,27 @@
 
 """ Distribution setup for ‘python-daemon’ library. """
 
-from __future__ import (absolute_import, unicode_literals)
 
 import os.path
-import pydoc
-import sys
 import unittest
 
-from setuptools import (setup, find_packages)
+from setuptools import setup
 
-import version
 
-
-fromlist_expects_type = str
-if sys.version_info < (3, 0):
-    fromlist_expects_type = bytes
-
-
-main_module_name = 'daemon'
-main_module_fromlist = list(map(fromlist_expects_type, [
-        '_metadata']))
-main_module = __import__(
-        main_module_name,
-        level=0, fromlist=main_module_fromlist)
-metadata = main_module._metadata
-
-(synopsis, long_description) = pydoc.splitdoc(pydoc.getdoc(main_module))
-
-
 def test_suite():
     """ Make the test suite for this code base. """
     loader = unittest.TestLoader()
     suite = loader.discover(os.path.curdir, pattern='test_*.py')
     return suite
 
-
-setup_kwargs = dict(
-        distclass=version.ChangelogAwareDistribution,
-        name=metadata.distribution_name,
-        packages=find_packages(exclude=["test"]),
-        cmdclass={
-            "write_version_info": version.WriteVersionInfoCommand,
-            "egg_info": version.EggInfoCommand,
-            "build": version.BuildCommand,
-            },
 
-        # Setuptools metadata.
-        zip_safe=False,
-        setup_requires=[
-            "docutils",
-            ],
-        test_suite="setup.test_suite",
-        tests_require=[
-            "unittest2 >=0.5.1",
-            "testtools",
-            "testscenarios >=0.4",
-            "mock >=1.3",
-            "docutils",
-            ],
-        install_requires=[
-            "setuptools",
-            "lockfile >=0.10",
-            ],
+setup(
+    setup_requires=["pbr"],
+    pbr=True,
+    test_suite='setup.test_suite'
+)
 
-        # PyPI metadata.
-        author=metadata.author_name,
-        author_email=metadata.author_email,
-        description=synopsis,
-        license=metadata.license,
-        keywords="daemon fork unix".split(),
-        url=metadata.url,
-        long_description=long_description,
-        classifiers=[
-            # Reference: <URL:https://pypi.org/classifiers/>
-            "Development Status :: 5 - Production/Stable",
-            "License :: OSI Approved :: Apache Software License",
-            "Operating System :: POSIX",
-            "Programming Language :: Python :: 2.7",
-            "Programming Language :: Python :: 3",
-            "Intended Audience :: Developers",
-            "Topic :: Software Development :: Libraries :: Python Modules",
-            ],
-        )
 
-# Docutils is only required for building, but Setuptools can't distinguish
-# dependencies properly.
-# See <URL:https://github.com/pypa/setuptools/issues/457>.
-setup_kwargs['install_requires'].append("docutils")
-
-
-if __name__ == '__main__':
-    setup(**setup_kwargs)
-
-
 # Copyright © 2008–2018 Ben Finney <ben+python@benfinney.id.au>
 #
 # This is free software: you may copy, modify, and/or distribute this work
