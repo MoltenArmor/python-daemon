a6965798eba80c51134ff43b5cfa965c356a0c5d
Ben Finney <ben+python@benfinney.id.au>
2023-10-20 16:12:20 +1100

    Delete unused Setuptools custom command classes.

--- util/packaging.py
+++ util/packaging.py
@@ -29,10 +29,7 @@ from .metadata import (
     parse_person_field,
     synopsis_and_description_from_docstring,
 )
-from .version import (
-    generate_version_info_from_changelog,
-    serialise_version_info_from_mapping,
-)
+from .version import generate_version_info_from_changelog
 
 
 def main_module_by_name(
@@ -68,77 +65,6 @@ def get_changelog_path(distribution, filename=changelog_filename):
     filepath = os.path.join(setup_dirname, filename)
 
     return filepath
-
-
-def has_changelog(command):
-    """ Return ``True`` iff the distribution's changelog file exists. """
-    result = False
-
-    changelog_path = get_changelog_path(command.distribution)
-    if changelog_path is not None:
-        if os.path.exists(changelog_path):
-            result = True
-
-    return result
-
-
-class BuildCommand(setuptools.command.build.build):
-    """ Custom ‘build’ command for this distribution. """
-
-    sub_commands = (
-            setuptools.command.build.build.sub_commands + [
-                ('egg_info', None),
-            ])
-
-
-class EggInfoCommand(setuptools.command.egg_info.egg_info):
-    """ Custom ‘egg_info’ command for this distribution. """
-
-    sub_commands = ([
-            ('write_version_info', has_changelog),
-            ] + setuptools.command.egg_info.egg_info.sub_commands)
-
-
-version_info_filename = "version_info.json"
-
-
-class WriteVersionInfoCommand(setuptools.command.egg_info.egg_info):
-    """ Setuptools command to serialise version info metadata. """
-
-    user_options = ([
-            ("changelog-path=", None,
-             "Filesystem path to the changelog document."),
-            ("outfile-path=", None,
-             "Filesystem path to the version info file."),
-            ] + setuptools.command.egg_info.egg_info.user_options)
-
-    def initialize_options(self):
-        """ Initialise command options to defaults. """
-        super().initialize_options()
-        self.changelog_path = None
-        self.outfile_path = None
-
-    def finalize_options(self):
-        """ Finalise command options before execution. """
-        self.set_undefined_options(
-                'build',
-                ('force', 'force'))
-
-        super().finalize_options()
-
-        if self.changelog_path is None:
-            self.changelog_path = get_changelog_path(self.distribution)
-
-        if self.outfile_path is None:
-            egg_dir = self.egg_info
-            self.outfile_path = os.path.join(egg_dir, version_info_filename)
-
-    def run(self):
-        """ Execute this command. """
-        version_info = generate_version_info_from_changelog(
-                self.changelog_path)
-        content = serialise_version_info_from_mapping(version_info)
-        self.write_file("version info", self.outfile_path, content)
 
 
 def derive_dist_description(
