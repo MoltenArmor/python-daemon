de3ac10e6f0424a8cbac409a6208fbc8c87ce5dd
Ben Finney <ben+python@benfinney.id.au>
2023-10-21 10:46:11 +1100

    Remove code for dynamically assembling custom version attributes.

--- daemon/_metadata.py
+++ daemon/_metadata.py
@@ -7,67 +7,9 @@
 
 """ Package metadata for the ‘python-daemon’ distribution. """
 
-import json
-
-import pkg_resources
-
 
 distribution_name = "python-daemon"
-version_info_filename = "version_info.json"
 
-
-def get_distribution(name):
-    """ Get the `Distribution` instance for distribution `name`.
-
-        :param name: The distribution name for the query.
-        :return: The `pkg_resources.Distribution` instance, or
-            ``None`` if the distribution instance is not found.
-        """
-    distribution = None
-    try:
-        distribution = pkg_resources.get_distribution(name)
-    except pkg_resources.DistributionNotFound:
-        pass
-
-    return distribution
-
-
-def get_distribution_version_info(
-        distribution, filename=version_info_filename):
-    """ Get the version info from the installed distribution.
-
-        :param distribution: The `pkg_resources.Distribution` instance
-            representing the Python package to interrogate.
-        :param filename: Base filename of the version info resource.
-        :return: The version info as a mapping of fields.
-
-        The version info is stored as a metadata file in the
-        `distribution`.
-
-        If the `distribution` is ``None``, or the version info
-        metadata file is not found, the return value mapping fields
-        have placeholder values.
-        """
-    version_info = {
-            'release_date': "UNKNOWN",
-            'version': "UNKNOWN",
-            'maintainer': "UNKNOWN",
-            }
-
-    if distribution is not None:
-        if distribution.has_metadata(filename):
-            content = distribution.get_metadata(filename)
-            version_info = json.loads(content)
-
-    return version_info
-
-
-distribution = get_distribution(distribution_name)
-version_info = get_distribution_version_info(distribution)
-
-version_installed = version_info['version']
-
-
 author_name = "Ben Finney"
 author_email = "ben+python@benfinney.id.au"
 author = "{name} <{email}>".format(name=author_name, email=author_email)
