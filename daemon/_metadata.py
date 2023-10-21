9315916f833dad513a3e2d9fc2e43ea44a6dfcfc
Ben Finney <ben+python@benfinney.id.au>
2023-10-21 11:06:16 +1100

    Remove the custom ‘daemon._metadata’ module.

--- daemon/_metadata.py
+++
@@ -1,23 +0,0 @@
-# daemon/_metadata.py
-# Part of ‘python-daemon’, an implementation of PEP 3143.
-#
-# This is free software, and you are welcome to redistribute it under
-# certain conditions; see the end of this file for copyright
-# information, grant of license, and disclaimer of warranty.
-
-""" Package metadata for the ‘python-daemon’ distribution. """
-
-
-# Copyright © 2008–2024 Ben Finney <ben+python@benfinney.id.au>
-#
-# This is free software: you may copy, modify, and/or distribute this work
-# under the terms of the Apache License, version 2.0 as published by the
-# Apache Software Foundation.
-# No warranty expressed or implied. See the file ‘LICENSE.ASF-2’ for details.
-
-
-# Local variables:
-# coding: utf-8
-# mode: python
-# End:
-# vim: fileencoding=utf-8 filetype=python :
