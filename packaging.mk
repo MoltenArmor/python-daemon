03cde8161d7c56621fea0e94d9811a8f204acb8b
Ben Finney <ben+python@benfinney.id.au>
2023-11-06 10:22:04 +1100

    Define ‘pip-confirm-test-dependencies-installed’ in ‘packaging’ module.

--- packaging.mk
+++ packaging.mk
@@ -21,9 +21,10 @@ PACKAGING_BUILD := $(PYTHON) -m ${PACKAGING_BUILD_MODULE}
 SETUPTOOLS_CONFIG_MODULE = setup
 PACKAGING_SETUP_MODULE_FILE := $(CURDIR)/${SETUPTOOLS_CONFIG_MODULE}.py
 
-PIP_DEVEL_DEPENDENCIES = .[devel]
-
 CODE_MODULES += ${PACKAGING_SETUP_MODULE_FILE}
+
+PIP_TEST_DEPENDENCIES = .[test]
+PIP_DEVEL_DEPENDENCIES = .[devel]
 
 GENERATED_FILES += $(CURDIR)/*.egg-info
 GENERATED_FILES += $(CURDIR)/.eggs/
@@ -54,6 +55,14 @@ pip-confirm-devel-dependencies-installed:
 .PHONY: pip-install-devel-dependencies
 pip-install-devel-dependencies:
 	$(PYTHON) -m pip install --no-input ${PIP_DEVEL_DEPENDENCIES}
+
+
+.PHONY: pip-confirm-test-dependencies-installed
+pip-confirm-test-dependencies-installed:
+	$(PYTHON) -m pip install \
+		--dry-run --no-input \
+		--no-index --no-build-isolation \
+		${PIP_TEST_DEPENDENCIES}
 
 
 .PHONY: packaging-build
