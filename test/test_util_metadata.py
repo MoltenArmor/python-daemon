6ee8a2de6c3990e177d03e3a4f6c25850dcf9321
Ben Finney <ben+python@benfinney.id.au>
2024-10-23 20:15:31 +1100

    Test and implement ‘util.metadata.description_fields_from_docstring’.

--- test/test_util_metadata.py
+++ test/test_util_metadata.py
@@ -124,6 +124,109 @@ class docstring_from_object_TestCase(
         self.assertEqual(self.expected_result, result)
 
 
+class description_fields_from_docstring_TestCase(
+        testscenarios.WithScenarios, testtools.TestCase):
+    """ Test cases for ‘description_fields_from_docstring’ function. """
+
+    function_to_test = staticmethod(
+        util.metadata.description_fields_from_docstring)
+
+    docstring_scenarios = [
+            ('single-line', {
+                'test_docstring': textwrap.dedent("""\
+                    Lorem ipsum, dolor sit amet.
+                    """),
+                'expected_synopsis': "Lorem ipsum, dolor sit amet.",
+                'expected_description': "",
+                }),
+            ('synopsis one-paragraph', {
+                'test_docstring': textwrap.dedent("""\
+                    Lorem ipsum, dolor sit amet.
+
+                    Donec et semper sapien, et faucibus felis. Nunc suscipit
+                    quam id lectus imperdiet varius. Praesent mattis arcu in
+                    sem laoreet, at tincidunt velit venenatis.
+                    """),
+                'expected_synopsis': "Lorem ipsum, dolor sit amet.",
+                'expected_description': textwrap.dedent("""\
+                    Donec et semper sapien, et faucibus felis. Nunc suscipit
+                    quam id lectus imperdiet varius. Praesent mattis arcu in
+                    sem laoreet, at tincidunt velit venenatis."""),
+                }),
+            ('synopsis three-paragraphs', {
+                'test_docstring': textwrap.dedent("""\
+                    Lorem ipsum, dolor sit amet.
+
+                    Ut ac ultrices turpis. Nam tellus ex, scelerisque ac
+                    tellus ac, placerat convallis erat. Nunc id mi libero.
+
+                    Donec et semper sapien, et faucibus felis. Nunc suscipit
+                    quam id lectus imperdiet varius. Praesent mattis arcu in
+                    sem laoreet, at tincidunt velit venenatis.
+
+                    Suspendisse potenti. Fusce egestas id quam non posuere.
+                    Maecenas egestas faucibus elit. Aliquam erat volutpat.
+                    """),
+                'expected_synopsis': "Lorem ipsum, dolor sit amet.",
+                'expected_description': textwrap.dedent("""\
+                    Ut ac ultrices turpis. Nam tellus ex, scelerisque ac
+                    tellus ac, placerat convallis erat. Nunc id mi libero.
+
+                    Donec et semper sapien, et faucibus felis. Nunc suscipit
+                    quam id lectus imperdiet varius. Praesent mattis arcu in
+                    sem laoreet, at tincidunt velit venenatis.
+
+                    Suspendisse potenti. Fusce egestas id quam non posuere.
+                    Maecenas egestas faucibus elit. Aliquam erat volutpat."""),
+                }),
+            ]
+
+    content_type_scenarios = [
+        ('default', {
+            'expected_content_type': "text/plain",
+        }),
+        ('text-plain', {
+            'test_content_type': "text/plain",
+            'expected_content_type': "text/plain",
+        }),
+        ('text-rst', {
+            'test_content_type': "text/x-rst",
+            'expected_content_type': "text/x-rst",
+        }),
+        ('text-markdown', {
+            'test_content_type': "text/x-markdown",
+            'expected_content_type': "text/x-markdown",
+        }),
+    ]
+
+    scenarios = testscenarios.multiply_scenarios(
+            docstring_scenarios, content_type_scenarios)
+
+    def setUp(self):
+        """ Set up fixtures for this test case. """
+        super().setUp()
+
+        self.expected_content_type = (
+            self.test_content_type if getattr(self, 'test_content_type', None)
+            else "text/plain")
+
+        self.test_args = [self.test_docstring]
+        self.test_kwargs = dict()
+        if hasattr(self, 'test_content_type'):
+            self.test_kwargs['content_type'] = self.test_content_type
+
+    def test_returns_expected_result(self):
+        """ Should return expected result. """
+        result = util.metadata.description_fields_from_docstring(
+                *self.test_args, **self.test_kwargs)
+        expected_result = util.metadata.DescriptionMetadata(
+                synopsis=self.expected_synopsis,
+                long_description=self.expected_description,
+                content_type=self.expected_content_type,
+        )
+        self.assertEqual(expected_result, result)
+
+
 class synopsis_and_description_from_docstring_TestCase(
         testscenarios.WithScenarios, testtools.TestCase):
     """ Test cases for ‘synopsis_and_description_from_docstring’ function. """
